aa4a0096f52249c317ad2c0d48a83e9c419760b5
tanjiro <toumanicoly53@gmail.com>
2026-01-16 13:20:13 +0000

    Implement service worker with caching strategy
    Add service worker for caching assets and offline support.

--- sw.js
+++ sw.js
@@ -1,3 +1,40 @@
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'nicoly-pro-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './logo.png',
+  './manifest.json'
+];
+
+// Installation du Service Worker et mise en cache des fichiers
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Mise en cache des ressources Nicoly-io');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Récupération des ressources (permet le mode hors-ligne)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
